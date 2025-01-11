063d6e4d88ee3061b82288481a8e152d6071ce84
Renan Joia <renanjoia@MacBook-Air-de-Renan.local>
2025-01-10 22:02:38 -0300

    Fix

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ app.get("/",(req,res)=>{
     return res.json({message: "Hello World"})
 })
 
+// Lidar com SIGTERM
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+        process.exit(0);
+    });
+});
+
+// Lidar com SIGINT
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+        process.exit(0);
+    });
+});
+
 app.listen(process.env.PORT || 2000, () => {
     console.log("Server is running...")
 })
