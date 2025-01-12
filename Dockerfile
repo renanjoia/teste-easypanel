b6a81a2feeb6cbe201aea4bb22c16fc6d5d4ae44
Renan Joia <renanjoia@MacBook-Air-de-Renan.local>
2025-01-12 12:28:19 -0300

    Fix

--- Dockerfile
+++ Dockerfile
@@ -10,11 +10,11 @@ RUN npm ci
 COPY . .
 
 # Configura variáveis de ambiente
-ENV PORT=3000
+ENV PORT=5000
 ENV NODE_ENV=production
 
 # Expõe a porta
-EXPOSE 2000
+EXPOSE 5000
 
 # Configura o healthcheck
 HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 \
--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require("express")
+const axios = require("axios")
 const app = express()
 
-app.get("/",(req,res)=>{
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+app.post("/",async(req,res)=>{
+//5511949343050
+    const phones = [
+        {phone:"5511940641960",message:"Olá Renan"},
+        {phone:"5511940641960",message:"Vi que está com alguns desafios para executar a tarefa de vínculo do tiny"},
+        {phone:"5511940641960",message:"Sou responsável pelo time de suporte e gostaria de te ajudar"},
+    ]
+
+    const requests = []
+    for (let i = 0; i < phones.length; i++) {
+        await sleep(3000)
+        const request = await axios.post(`https://evolution.bdanalytics.com.br/message/sendText/renanjoia`,{"number":`${phones[i].phone}@s.whatsapp.net`,
+	"text":`${phones[i].message}}`},{headers: {apiKey:"D8ADB0E69808-45C4-BA16-81768364F729"}})
+        
+        requests.push(request.data)
+    }
+
     return res.json({message: "Hello World"})
+})
+
+app.get("/",(req,res)=>{
+    return res.json({name:"Hello World"})
 })
 
 // Lidar com SIGTERM
@@ -23,7 +46,7 @@ process.on('SIGINT', () => {
     });
 });
 
-app.listen(process.env.PORT || 2000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running...")
 })
 
